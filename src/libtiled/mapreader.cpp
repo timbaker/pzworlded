533e1f4e4f68f63a2506b32d3d193d1c8251b062
Tim Baker <treectrl@users.sf.net>
2013-05-31 11:05:27 -0700

    Removed qApp->processEvents call in MapReader.
    Maps are loaded in threads now.

--- src/libtiled/mapreader.cpp
+++ src/libtiled/mapreader.cpp
@@ -651,12 +651,6 @@ void MapReaderPrivate::decodeCSVLayerData(TileLayer *tileLayer, const QString &t
         }
         tileLayer->setCell(x, y, cellForGid(gid));
     }
-
-    // Hack to keep the app responsive.
-    // TODO: Move map reading to a worker thread. Only issue is tileset images
-    // cannot be accessed outside the GUI thread.
-    qApp->processEvents(QEventLoop::ExcludeUserInputEvents);
-
 #elif 0
     QString trimText = text.trimmed();
     static QVector<int> tiles;
