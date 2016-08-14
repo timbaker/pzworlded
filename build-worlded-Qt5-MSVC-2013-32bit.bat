f11050708cad23b3500ab44dfdc43e558830bdee
Tim Baker <treectrl@users.sf.net>
2016-08-13 17:08:18 -0700

    Updated to Qt 5.7.

--- build-worlded-Qt5-MSVC-2013-32bit.bat
+++ build-worlded-Qt5-MSVC-2013-32bit.bat
@@ -1,7 +1,7 @@
 call "D:\VisualStudioCommunity2013\VC\vcvarsall.bat" x86
 mkdir C:\Programming\PZWorldEd\dist32
 cd C:\Programming\PZWorldEd\dist32
-"C:\Programming\QtSDK2015\5.5\msvc2013\bin\qmake.exe" C:\Programming\PZWorldEd\pzworlded\PZWorldEd.pro -r -spec win32-msvc2013 "CONFIG+=release"
+"C:\Programming\QtSDK2015\5.7\msvc2013\bin\qmake.exe" C:\Programming\PZWorldEd\pzworlded\PZWorldEd.pro -r -spec win32-msvc2013 "CONFIG+=release"
 "C:\Programming\QtSDK2015\Tools\QtCreator\bin\jom.exe"
 "C:\Programming\TclTk\8.5.x\32bit-mingw\bin\tclsh85.exe" C:\Programming\PZWorldEd\pzworlded\dist.tcl 32bit
 PAUSE
