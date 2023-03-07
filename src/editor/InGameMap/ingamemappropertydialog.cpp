9b9d9df6f4ddf242b3eb2860a34f94dc7abb5b15
Tim Baker <timnbaker@gmail.com>
2023-03-06 17:13:24 -0800

    Added auto-complete to the in-game-map feature-property editor.

--- src/editor/InGameMap/ingamemappropertydialog.cpp
+++ src/editor/InGameMap/ingamemappropertydialog.cpp
@@ -1,13 +1,35 @@
 #include "ingamemappropertydialog.h"
 #include "ui_ingamemappropertydialog.h"
 
-#include "ingamemapcell.h"
+#include "celldocument.h"
+#include "documentmanager.h"
+#include "document.h"
+#include "world.h"
+#include "worlddocument.h"
+
+#include <QCompleter>
 
 InGameMapPropertyDialog::InGameMapPropertyDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::InGameMapPropertyDialog)
 {
     ui->setupUi(this);
+    QSet<QString> keySet, valueSet;
+    Document *doc = DocumentManager::instance()->currentDocument();
+    WorldDocument *worldDoc = doc->asWorldDocument();
+    CellDocument *cellDoc = doc->asCellDocument();
+    World *world = worldDoc ? worldDoc->world() : cellDoc->world();
+    for (auto* cell : world->cells()) {
+        const InGameMapFeatures &features = cell->inGameMap().features();
+        for (const auto& feature : features) {
+            for (const auto& prop : qAsConst(feature->properties())) {
+                keySet += prop.mKey;
+                valueSet += prop.mValue;
+            }
+        }
+    }
+    ui->keyEdit->setCompleter(new QCompleter(QStringList(keySet.begin(), keySet.end()), this));
+    ui->valueEdit->setCompleter(new QCompleter(QStringList(valueSet.begin(), valueSet.end()), this));
 }
 
 InGameMapPropertyDialog::~InGameMapPropertyDialog()
