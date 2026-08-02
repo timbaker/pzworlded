752a5426939508768dce2846bc7b49275e593312
Tim Baker <timnbaker@gmail.com>
2026-08-02 13:15:11 -0700

    Removed macdeployqt.sh. Changed make-dist-mac.sh to remove symlinks

--- dist/macdeployqt.sh
+++
@@ -1,3 +0,0 @@
-cd ~/Programming/pzworlded/Qt_6_11_1_for_macOS_Release/bin/
-~/Qt/6.11.1/macOS/bin/macdeployqt PZWorldEd.app
-otool -L PZWorldEd.app/Contents/MacOS/PZWorldEd
--- dist/make-dist-mac.sh
+++ dist/make-dist-mac.sh
@@ -1,12 +1,23 @@
+#!/bin/bash
 SRC=$(pwd)/..
-BUILD=$SRC/../Qt_6_11_1_for_macOS_Release/bin/
-DESTROOT=$SRC/../../ProjectZomboid
+BUILD=$(realpath $SRC/../Qt_6_11_1_for_macOS_Release/bin/)
+DESTROOT=$(realpath $SRC/../../ProjectZomboid)
 DEST=$DESTROOT/WorldEd
 
 mkdir -p $DESTROOT
 mkdir -p $DEST
 
+rm -rf $DEST/PZWorldEd.app
 cp -a $BUILD/PZWorldEd.app $DEST
+
+# Add all necessary Qt libraries
+~/Qt/6.11.1/macOS/bin/macdeployqt $DEST/PZWorldEd.app
+
+# Remove all symlinks, they won't work on Steam
+find $DEST/PZWorldEd.app/Contents -type l -delete
+
+mv $DEST/PZWorldEd.app/Contents/Frameworks/libtiled.1.0.0.dylib $DEST/PZWorldEd.app/Contents/Frameworks/libtiled.1.dylib
+mv $DEST/PZWorldEd.app/Contents/Frameworks/libzlib1.1.0.0.dylib $DEST/PZWorldEd.app/Contents/Frameworks/libzlib1.1.dylib
 
 cp -a $SRC/LICENSE.BSD $DEST
 cp -a $SRC/LICENSE.GPL $DEST
