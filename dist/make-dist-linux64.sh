32b039a3b2cd3278530e2e580a276448d9bf639c
Tim Baker <timbaker@theindiestone.com>
2026-08-02 13:45:25 -0700

    Fixed make-dist-linux64.sh

--- dist/make-dist-linux64.sh
+++ dist/make-dist-linux64.sh
@@ -4,8 +4,8 @@ APPIMAGE=$SRC/../linuxdeploy/PZWorldEd-x86_64.AppImage
 DESTROOT=$SRC/../dist-tiled-docker
 DEST=$DESTROOT/WorldEd
 
-mkdir $DESTROOT
-mkdir $DEST
+mkdir -p $DESTROOT
+mkdir -p $DEST
 cp -a $APPIMAGE $DEST
 
 cp -a $SRC/Blends.txt $DESTROOT/TileZed/share/tilezed/config
@@ -15,7 +15,7 @@ cp -a $SRC/WorldDefaults.txt $DESTROOT/TileZed/share/tilezed/config
 cp -a $SRC/dist/PZWorldEd-x86_64.AppImage.sh $DEST
 chmod +x $DEST/PZWorldEd-x86_64.AppImage.sh
 
-cp -a $SRC/LICENSE.BSD.txt $DEST
-cp -a $SRC/LICENSE.GPL.txt $DEST
+cp -a $SRC/LICENSE.BSD $DEST
+cp -a $SRC/LICENSE.GPL $DEST
 cp -a $SRC/LICENSE.QT6 $DEST
 
